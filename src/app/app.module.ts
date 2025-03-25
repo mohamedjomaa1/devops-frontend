b00dc2bc97d98997e39ee8815cbdda5b67279376
Mohamed Jomaa <mohamed.jomaa@esprim.tn>
2025-03-26 00:52:59 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';  
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculComponent } from './calcul/calcul.component';
+import { HeureSupComponent } from './heure-sup/heure-sup.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculComponent,
+    HeureSupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule  
+
   ],
   providers: [
     provideClientHydration(withEventReplay())
